42ef2d5e955c05f409deee4a96650bd7cf5fe298
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2023-05-13 01:18:18 -0400

    add test

--- DeepgramAudioTranscription/ApiAudiotext/tests.py
+++ DeepgramAudioTranscription/ApiAudiotext/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
 # Create your tests here.
+
+class TranscriptionViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('transcription')
+
+    def test_post_with_valid_file(self):
+        with open('./../files/audio.mp3', 'rb') as f:
+            response = self.client.post(self.url, {'audio': f})
+            self.assertEqual(response.status_code, 200)
+
+    def test_post_with_invalid_file(self):
+        with open('./../files/image.png', 'rb') as f:
+            response = self.client.post(self.url, {'audio': f})
+            self.assertEqual(response.status_code, 400)
+
+    def test_post_with_empty_request(self):
+        response = self.client.post(self.url)
+        self.assertEqual(response.status_code, 400)
--- DeepgramAudioTranscription/ApiAudiotext/views.py
+++ DeepgramAudioTranscription/ApiAudiotext/views.py
@@ -3,11 +3,14 @@ from django.http.response import JsonResponse
 from .controllers import TranscriberController
 import os
 from dotenv import load_dotenv
-
+from django.middleware.csrf import get_token
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 load_dotenv()
 # Create your views here.
 
 
+@method_decorator(csrf_exempt, name='dispatch')
 class TranscriptionView(View):
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
@@ -15,13 +18,21 @@ class TranscriptionView(View):
 
     async def post(self, request):
         try:
-            print(request.FILES)
             audio_file = request.FILES['audio']
+            if not audio_file.content_type.startswith('audio/'):
+                return JsonResponse({'error': 'Invalid file format. Only audio files.'}, status=400)
             minetype = audio_file.content_type
-        except Exception as e:
-            return JsonResponse({'error': str(e)}, status=400)
+        except KeyError:
+            return JsonResponse({'error': 'Audio file not found.'}, status=400)
         response = await self.transcriber.transcribe(audio_file.read(), minetype)
-        return JsonResponse(response, status=200)
+        transcription = response.get('results').get(
+            'channels')[0].get('alternatives')[0].get('transcript')
+        return JsonResponse({"transcript": transcription}, status=200)
 
-    async def get(self, request):
-        return JsonResponse({'message': 'Hello World'}, status=200)
+
+"""
+curl -c cookie.txt -X GET http://localhost:8000/api/transcription/ && 
+csrftoken=$(grep -oP '(?<=csrftoken\s)[^;]+' cookie.txt) &&
+curl -X POST -H 'X-CSRFToken: $csrftoken' -F 'audio=@/home/martindev/Documentos/github/apiAudioText/audios/cuentan_que_hace_mucho_tiempo.mp3' http://localhost:8000/api/transcription/
+ 
+"""
